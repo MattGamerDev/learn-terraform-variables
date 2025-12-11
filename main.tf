bebf79d4f07f47a3f8fc191da6e487745e0fb4c9
LAPTOP-MAA41VL3\mtthw <mtthwgamb@gmail.com>
2025-12-11 10:49:01 -0700

    fix missing variables and output

--- main.tf
+++ main.tf
@@ -127,6 +127,6 @@ module "ec2_instances" {
 
 module "terraform-aws-s3-bucket-mg" {
   source  = "app.terraform.io/policy-as-code-training/terraform-aws-s3-bucket-mg/aws"
-  version = "1.1.0"
+  version = "1.1.2"
   bucket_name = "mghcps3bucket"
 }
